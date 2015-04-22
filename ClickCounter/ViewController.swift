6d10db03b228e7a3d1e7f59d0cfb3275b5bc735d
Shahbaz Javeed <sjaveed@gmail.com>
2015-04-22 17:13:05 +0400

    * Initial implementation with a single button and label.

--- ClickCounter/ViewController.swift
+++ ClickCounter/ViewController.swift
@@ -10,16 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var count = 0
+    var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+        // Label to display click count
 
+        self.label = UILabel()
+        self.label.frame = CGRectMake(150, 150, 60, 60)
+        self.label.text = "0"
+        
+        self.view.addSubview(self.label)
+        
+        // Button whose clicks to count
+        
+        var button = UIButton()
+        button.frame = CGRectMake(150, 250, 60, 60)
+        button.setTitle("Click", forState: .Normal)
+        button.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        button.addTarget(self, action: "incrementCounter", forControlEvents: UIControlEvents.TouchUpInside)
+
+        self.view.addSubview(button)
+    }
+    
+    func incrementCounter() {
+        self.count++
+        self.label.text = "\(self.count)"
+    }
 
 }
 
